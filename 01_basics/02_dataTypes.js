192f946d83297c2ee37d7024a58c9432e8d57f23
shalabhsahu2001 <70065169+shalabhsahu2001@users.noreply.github.com>
2024-05-29 19:32:47 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+// alert(3 + 3) // we are using NODE JS, not BROWSER
+let name = "Shalabh";
+let age = 21;
+let isLoggedIn = false;
+/* primitve data types
+//number 2 to power 53
+//bigInt
+//boolean = true/false
+//string = ""
+// null => standalone value // object hai
+//undefined => special // apne aap me ek type h
+//symbol => unique
+
+*/
+
+//object
+
+console.log(typeof undefined);

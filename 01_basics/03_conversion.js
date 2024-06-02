5edfc4537d15a3a9a79cc138c0aeacc692e691d5
shalabhsahu2001 <70065169+shalabhsahu2001@users.noreply.github.com>
2024-06-02 18:19:44 +0000

    Datatype Conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,30 @@
+let score = "Hitesh"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score);
+console.log(valueInNumber)
+console.log(typeof valueInNumber)
+
+
+// "33" => 33
+
+// "33abs" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => boolean me true, 0 boolean me false
+// empty string boolean me false;
+// non-empty string true;
+
+let someNumber = 20
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof(stringNumber))
+
+// number se string me convert ho jaata

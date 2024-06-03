7e4a2b672775410f9fa391302b15c6a03d32dc25
shalabhsahu2001 <70065169+shalabhsahu2001@users.noreply.github.com>
2024-06-03 08:50:15 +0000

    String conversion

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,11 +1,11 @@
 let score = "Hitesh"
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
 let valueInNumber = Number(score);
-console.log(valueInNumber)
-console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
 
 
 // "33" => 33
@@ -15,7 +15,7 @@ console.log(typeof valueInNumber)
 
 let isLoggedIn = ""
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1 => boolean me true, 0 boolean me false
 // empty string boolean me false;
@@ -24,7 +24,39 @@ console.log(booleanIsLoggedIn);
 let someNumber = 20
 let stringNumber = String(someNumber)
 
-console.log(stringNumber)
-console.log(typeof(stringNumber))
+// console.log(stringNumber)
+// console.log(typeof(stringNumber))
 
 // number se string me convert ho jaata
+
+/**********operations**************/
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+// (**) power 
+
+let str1 = "Hello"
+let str2 = " Shalabh"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// jo type phle hoga output usme convert ho jaega
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+//wrong methodology to write code
+console.log(num1);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)

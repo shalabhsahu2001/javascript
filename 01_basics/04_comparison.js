091d3d590305123c60829e9846c63c870cb95e70
shalabhsahu2001 <70065169+shalabhsahu2001@users.noreply.github.com>
2024-06-16 15:30:31 +0000

    comparison in js revision

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -4,21 +4,23 @@
 // console.log("02" > 1)
 
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
 
 
 //reason is that and equlity check == comparison > < >= <= work differently.
 //comparison conver null to a number, treating it as 0.
 //That's why (3) null >= 0 is true and (1) null > 0 is false
 
-console.log(undefined == 0);
-console.log(undefined == 0);
-console.log(undefined == 0);
+// console.log(undefined == 0);
+// console.log(undefined == 0);
+// console.log(undefined == 0);
 
-//undefine sabme false deta hai
+// //undefine sabme false deta hai
 
-/* Strict check (===) */
-//data type same hona chaiye
-console.log("2" === 2);
+// /* Strict check (===) */
+// //data type same hona chaiye
+// console.log("2" === 2);
+
+console.log("3" === "1");
